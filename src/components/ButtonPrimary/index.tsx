a4ce30f69b9474265b1655e4b04373074ddfca10
Murilo Martins <murilomartins@outlook.com>
2023-02-17 17:00:33 -0300

    :construction: Static Components

---
+++ src/components/ButtonPrimary/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+function ButtonPrimary() {
+  return (
+    <>
+      <div className="dsc-btn">Comprar</div>
+    </>
+  );
+}
+
+export default ButtonPrimary;
---
+++ src/components/ButtonSecondary/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+function ButtonSecondary() {
+  return (
+    <>
+      <div className="dsc-btn">Início</div>
+    </>
+  );
+}
+
+export default ButtonSecondary;
