ca265600701458fcc42c7d33c227a0832e1fa050
Murilo Martins <murilomartins@outlook.com>
2023-02-23 09:52:35 -0300

    :construction: Product and Category models and props

--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -3,6 +3,22 @@ import HeaderClient from "../../components/HeaderClient";
 import ButtonPrimary from "../../components/ButtonPrimary";
 import ButtonSecondary from "../../components/ButtonSecondary";
 import ProductDetailsCard from "../../components/ProductDetailsCard";
+import { ProductDTO } from "../../models/product";
+
+const product: ProductDTO = {
+  id: 2,
+  name: "Smart TV",
+  description:
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, deleniti animi ullam quod nam quia.",
+  imgUrl:
+    "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
+  price: 2500.99,
+  categories: [
+    { id: 2, name: "Eletrônicos" },
+    { id: 3, name: "Computadores" },
+    { id: 4, name: "Importados" },
+  ],
+};
 
 function ProductDetails() {
   return (
@@ -11,7 +27,7 @@ function ProductDetails() {
       <main>
         <section id="product-details-section" className="dsc-container">
           <div className="dsc-card dsc-mb20">
-            <ProductDetailsCard />
+            <ProductDetailsCard product={product} />
           </div>
           <div className="dsc-btn-page-container">
             <ButtonPrimary />
