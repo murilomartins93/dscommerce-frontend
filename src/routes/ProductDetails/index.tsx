506744413c24136b72f2f385b6cbf2d99d654c26
Murilo Martins <murilomartins@outlook.com>
2023-02-24 15:29:09 -0300

    :books: Adding links

--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -2,26 +2,25 @@ import "./styles.css";
 import Button from "../../components/Button";
 import ProductDetailsCard from "../../components/ProductDetailsCard";
 import * as productService from "../../services/product-service";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function ProductDetails() {
-
   const params = useParams();
   const product = productService.findById(Number(params.productId));
 
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
-        {
-          product && 
+        {product && (
           <div className="dsc-card dsc-mb20">
             <ProductDetailsCard product={product} />
           </div>
-          
-        }
+        )}
         <div className="dsc-btn-page-container">
           <Button text="Comprar" />
-          <Button text="Início" />
+          <Link to="/">
+            <Button text="Início" />
+          </Link>
         </div>
       </section>
     </main>
