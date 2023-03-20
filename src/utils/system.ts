1823a5b6f54a49ae4a8156855cc984e7db29ba6e
Murilo Martins <murilomartins@outlook.com>
2023-03-19 22:17:46 -0300

    :rocket: Deploy ready

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,10 +1,8 @@
-export const BASE_URL = "http://localhost:8080";
-
+// LocalStorage
 export const CART_KEY = "com.devsuperior.dscommerce/Cart";
-
 export const TOKEN_KEY = "com.devsuperior.dscommerce/Token";
 
-export const CLIENT_ID = "myclientid";
-
-export const CLIENT_SECRET = "myclientsecret";
-
+// Environment variables
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID ?? "myclientid";
+export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET ?? "myclientsecret";
