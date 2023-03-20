197cbabda1061fc6152422f24275fd9353a01792
Murilo Martins <murilomartins@outlook.com>
2023-03-19 22:54:02 -0300

    :sparkles: Added HeaderAdmin Catalog route

--- src/components/HeaderAdmin/index.tsx
+++ src/components/HeaderAdmin/index.tsx
@@ -2,6 +2,7 @@ import "./styles.css";
 import { Link, NavLink } from "react-router-dom";
 import productsIcon from "../../assets/products.svg";
 import homeIcon from "../../assets/home.svg";
+import catalogIcon from "../../assets/catalog.svg";
 import LoggedUser from "../LoggedUser";
 
 function HeaderAdmin() {
@@ -13,12 +14,19 @@ function HeaderAdmin() {
         </Link>
         <div className="dsc-navbar-right">
           <div className="dsc-menu-items-container">
+            
             <NavLink to="/admin/home" className={({isActive}) => isActive ? "dsc-menu-item-active" : ""}>
               <div className="dsc-menu-item">
                 <img src={homeIcon} alt="Início" />
                 <p>Início</p>
               </div>
             </NavLink>
+            <Link to="/catalog">
+              <div className="dsc-menu-item">
+                <img src={catalogIcon} alt="Catálogo" />
+                <p>Catálogo</p>
+              </div>
+            </Link>
             <NavLink to="/admin/products" className={({isActive}) => isActive ? "dsc-menu-item-active" : ""}>
               <div className="dsc-menu-item">
                 <img src={productsIcon} alt="Cadastro de produtos" />
